0a12c05371deaa2ae6912f5ffd3f5f9c1cca1b46
learningpath2025 <learningpath2025@outlook.com>
2025-11-10 11:18:53 -0600

    initial commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Assignment12._1._1
+{
+    internal class Program
+    {
+        public static bool IsConstructable(string ransomNote, string magazine)
+        {
+            char[] ransomArr = ransomNote.ToCharArray();
+            foreach (char c in ransomArr)
+            {
+                int index = magazine.IndexOf(c);
+                if (index != -1)
+                {
+                    magazine = magazine.Remove(index, 1);
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        static void Main(string[] args)
+        {
+            string ransomNote = "aab";
+            string magazine = "baa";
+            Console.WriteLine($"Output: {IsConstructable(ransomNote, magazine)}");
+        }
+    }
+}
